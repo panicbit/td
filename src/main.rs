62d305ead0bc345c5b7c80771689d00855371fc9
sarna <sarna.dev@protonmail.com>
2018-10-04 11:21:09 +0200

    Add basic info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("td")
+        .version("0.1.0")
+        .author("sarna <sarna.dev@protonmail.com>")
+        .about("A to-do list for the command line")
+        .get_matches();
 }
