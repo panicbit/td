e43d42141da962076efe3c6a0f3913538b10cd5c
sarna <sarna.dev@protonmail.com>
2018-10-05 13:06:20 +0200

    Add persistence
    Now the tasks are saved in ~/.local/share/td/{date} {task name}

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use std::fs::{create_dir, File};
+use std::io::prelude::*;
 use std::io::{self, Write};
+use std::path::Path;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate ron;
+extern crate serde;
+
+extern crate dirs;
+use dirs::data_local_dir;
+
+extern crate chrono;
+use chrono::Local;
 
 extern crate clap;
-
 use clap::{App, Arg, SubCommand};
 
 const VERSION: &str = "0.1.0";
@@ -14,6 +27,9 @@ fn first_letter_to_upper(s: &str) -> String {
     }
 }
 
+// TODO: is the task done
+// TODO: description (maybe)
+#[derive(Serialize, Deserialize, Debug)]
 struct Task {
     task: String,
     outcome: String,
@@ -60,6 +76,25 @@ impl Task {
         println!("in order to {}", self.outcome);
         println!("because I want to {}", self.desire);
     }
+
+    fn save(&self) -> std::io::Result<()> {
+        let data_local_dir = match data_local_dir() {
+            Some(dir) => dir,
+            None => panic!("Could not open the local data directory"),
+        };
+        let td_path = Path::new(&data_local_dir).join("td");
+        if !td_path.exists() {
+            create_dir(&td_path)?;
+        }
+        let now = Local::now().to_rfc3339(); // TODO: make it nicer (maybe)
+                                             // TODO: consider replacing spaces with something else
+        let filename = format!("{} {}", now, &self.task);
+        let task_path_string = td_path.join(filename); // that's atrocious
+        let task_path = Path::new(&task_path_string);
+        let mut file = File::create(&task_path)?;
+        file.write_all(ron::ser::to_string(&self).unwrap().as_bytes())?;
+        Ok(())
+    }
 }
 
 fn main() {
@@ -74,6 +109,9 @@ fn main() {
         ).get_matches();
     if let Some(matches) = matches.subcommand_matches("new") {
         let task = new_task();
-        // task.print();
+        match task.save() {
+            Ok(_) => (),
+            Err(why) => println!("{}", why),
+        }
     }
 }
