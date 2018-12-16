8390ca326ad495fea7e57bebeab4dc7e32bd804b
panicbit <panicbit.dev@gmail.com>
2018-12-16 19:53:29 +0100

    Split cli command handling into functions

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate serde;
 extern crate clap;
 extern crate dirs;
 
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg, SubCommand, ArgMatches};
 
 mod task;
 use task::Task;
@@ -37,30 +37,37 @@ fn main() {
                 ),
         ).get_matches();
 
-    if let Some(_matches) = matches.subcommand_matches("new") {
-        let task = Task::new();
+    match matches.subcommand() {
+        ("new", Some(matches)) => command_new(matches),
+        ("list", Some(matches)) => command_list(matches),
+        ("delete", Some(matches)) => command_delete(matches),
+        _ => {},
+    }
+}
 
-        match task.save() {
+fn command_new(_matches: &ArgMatches) {
+    let task = Task::new();
+
+    match task.save() {
+        Ok(_) => (),
+        Err(why) => println!("{}", why),
+    }
+}
+
+fn command_list(_matches: &ArgMatches) {
+    match Task::list_all() {
+        Ok(_) => (),
+        Err(why) => println!("{}", why),
+    }
+}
+
+fn command_delete(matches: &ArgMatches) {
+    if let Some(task) = matches.value_of("task") {
+        match Task::delete(task) {
             Ok(_) => (),
             Err(why) => println!("{}", why),
         }
-    }
-
-    if let Some(_matches) = matches.subcommand_matches("list") {
-        match Task::list_all() {
-            Ok(_) => (),
-            Err(why) => println!("{}", why),
-        }
-    }
-
-    if let Some(matches) = matches.subcommand_matches("delete") {
-        if let Some(task) = matches.value_of("task") {
-            match Task::delete(task) {
-                Ok(_) => (),
-                Err(why) => println!("{}", why),
-            }
-        } else {
-            println!("Which one? Try e.g. `td delete 1`");
-        }
+    } else {
+        println!("Which one? Try e.g. `td delete 1`");
     }
 }
